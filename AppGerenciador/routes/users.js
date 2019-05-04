01bbe46cb9b09a5ed0d02fb1959b17edaa32eac6
Aluno <Aluno@Digital.net>
2019-05-03 21:14:46 -0300

    aaaaaa

--- AppGerenciador/routes/users.js
+++ AppGerenciador/routes/users.js
@@ -20,13 +20,24 @@ module.exports = (app) =>{
     });
     
     route.post((req, res)=>{
-        db.insert(req.body,(err, user)=>{
-            if(err){
-                app.utils.error.send(err, req, res);
-            } else{
-                res.status(200).json(user);
-            }
-        });
+        req.assert('name', 'nome é obrigatório').notEmpty();
+        req.assert('email', 'email é INVALIDO').notEmpty();
+        req.assert('password', 'senha é obrigatória').notEmpty();
+
+        let error = req.validationErrors();
+        if (error) {
+            app.utils.error.send(error, req, res);
+            
+        }else{
+            db.insert(req.body,(err, user)=>{
+                if(err){
+                    app.utils.error.send(err, req, res);
+                } else{
+                    res.status(200).json(user);
+                }
+            });
+        }
+        
     });
 
     let routeId = app.route('/users/:id');
@@ -48,6 +59,17 @@ module.exports = (app) =>{
                 res.status(200).json(Object.assign(req.body, req.params));
             }
         })
-    })
+    });
+
+    routeId.delete((req, res) => {
+        db.remove({_id: req.params.id}, {}, err=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else{
+                res.status(200).json(Object.assign(req.body, req.params));
+            }
+        });
+    });
+
 
 }; 
